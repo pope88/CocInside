9a10141d0c0cce16accae40eaa65527ac5b601a8
pope2 <pope2@163.com>
2013-06-26 08:03:44 +0800

    extens

--- CocTest/Classes/View/Scene/CMenuScene.cpp
+++ CocTest/Classes/View/Scene/CMenuScene.cpp
@@ -7,35 +7,36 @@ namespace View
 {
 	void CMenuScene::InitScene()
 	{
-		CCSize visibleSize = CCDirector::sharedDirector()->getVisibleSize();
-		CCPoint origin = CCDirector::sharedDirector()->getVisibleOrigin();
+		//CCSize visibleSize = CCDirector::sharedDirector()->getVisibleSize();
+		//CCPoint origin = CCDirector::sharedDirector()->getVisibleOrigin();
 
-		CCLayer *testLayer = CCLayer::create();
+		//CCLayer *testLayer = CCLayer::create();
 
-		//try 九宫格
-		//CCRect rect = CCRectMake(0, 0, 320, 480);
-		//CCRect rectInsets = CCRectMake(1, 118, 1, 30);
-		//CCSize winRect = CCSizeMake(visibleSize.width, visibleSize.height);
-		//CCScale9Sprite *pbgSprite = CCScale9Sprite::create(s_pPathBackGround2, rect, rectInsets);
-		//pbgSprite->setContentSize(winRect);
-		CCSprite  *pbgSprite = CCSprite::create(s_pPathBackGround2);
+		////try 九宫格
+		////CCRect rect = CCRectMake(0, 0, 320, 480);
+		////CCRect rectInsets = CCRectMake(1, 118, 1, 30);
+		////CCSize winRect = CCSizeMake(visibleSize.width, visibleSize.height);
+		////CCScale9Sprite *pbgSprite = CCScale9Sprite::create(s_pPathBackGround2, rect, rectInsets);
+		////pbgSprite->setContentSize(winRect);
+		//CCSprite  *pbgSprite = CCSprite::create(s_pPathBackGround2);
 
-		CCLabelTTF* label = CCLabelTTF::create("second scene", "Arial", 20);
-		//#endif
-		CCMenuItemLabel* pMenuItem = CCMenuItemLabel::create(label, this, menu_selector(CMenuScene::OnTextmenu_Click));
+		//CCLabelTTF* label = CCLabelTTF::create("second scene", "Arial", 20);
+		////#endif
+		//CCMenuItemLabel* pMenuItem = CCMenuItemLabel::create(label, this, menu_selector(CMenuScene::OnTextmenu_Click));
 
 
-		CCMenu* pMenu =CCMenu::create(pMenuItem, NULL);
+		//CCMenu* pMenu =CCMenu::create(pMenuItem, NULL);
 
-		pbgSprite->setPosition(ccp(origin.x + visibleSize.width/2 , origin.y + visibleSize.height/2));
-		pMenu->setPosition(ccp(origin.x + visibleSize.width/2 , origin.y + visibleSize.height/2));
+		//pbgSprite->setPosition(ccp(origin.x + visibleSize.width/2 , origin.y + visibleSize.height/2));
+		//pMenu->setPosition(ccp(origin.x + visibleSize.width/2 , origin.y + visibleSize.height/2));
 
-		pbgSprite->setScale(2.0);
-		testLayer->addChild(pbgSprite);
-		testLayer->addChild(pMenu);
-		this->addChild(testLayer);
+		//pbgSprite->setScale(2.0);
+		//testLayer->addChild(pbgSprite);
+		//testLayer->addChild(pMenu);
+		//this->addChild(testLayer);
 
-		testLayer->setTouchEnabled(true);
+		//testLayer->setTouchEnabled(true);
+		
 	}
 
 
